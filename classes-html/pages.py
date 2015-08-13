476d11ab0690df503fb5ceb4cad6fde2af5f1df6
dellbby <dellbby4@yahoo.com>
2015-08-12 20:16:09 -0700

    d

--- classes-html/pages.py
+++ classes-html/pages.py
@@ -1,19 +1,13 @@
-
-import webapp2
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        p = Page()
-        self.response.write(p.print_out()) #prints out to browser
-
 class Page(object):
     def __init__(self):
         self.title = "Welcome!"
+        self.css = "css/style.css" #stylesheet
         self.head = """
 <!DOCTYPE HTML>
 <html>
     <head>
         <title>{self.title}</title>
+        <link href = "{self.css}" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         """
@@ -27,8 +21,3 @@ class Page(object):
         all = self.head + self.body + self.close
         all = all.format(**locals())
         return all
-
-
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
