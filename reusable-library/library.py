e4d26ab7bcbfc9e036960702c628168a10e33598
dellbby <dellbby4@yahoo.com>
2015-08-20 19:17:23 -0700

    grades
    10

--- reusable-library/library.py
+++ reusable-library/library.py
@@ -1,23 +1,64 @@
 class Grades(object):
     def __init__(self):
     #Grade point scale, return number for each grade
-    def conv(g):
-        if g == 'A' :
-            return 5
-        elif g == 'B':
-            return 4
-        elif g == "C":
-            return 3
-        elif g == 'D':
-            return 2
-        elif g == 'F'
-            return 0
+    def scale(self):
+        #variables of grades and credits
+        a = 5.0
+        b = 4.0
+        c = 3.0
+        d = 2.0
+        f = 0
 
-    def gpa (c_no):
-        v  = 0
-        tlu = 0
-        t = 0
+    if history_grade == 'a':
+        pass
+    elif history_grade == 'b':
+        pass
+    elif history_grade == 'c':
+        pass
+    elif history_grade == 'd':
+        pass
+    elif history_grade == 'f':
+        pass
+    else:
+        print "Your grade is invalid"
 
-        while v!=c_no:
-            v = v +1
-            c =
+
+    if english_grade == 'a':
+        pass
+    elif english_grade == b:
+        pass
+    elif english_grade == c:
+        pass
+    elif english_grade == d:
+        pass
+    elif english_grade == f:
+        pass
+    else:
+        print "Your grade is invalid"
+
+
+    if math_grade == a:
+        pass
+    elif math_grade == b:
+        pass
+    elif math_grade == c:
+        pass
+    elif math_grade == d:
+        pass
+    elif math_grade == f:
+        pass
+    else:
+        print "Your grade is invalid"
+
+    if biology_grade == a:
+        pass
+    elif biology_grade == b:
+        pass
+    elif biology_grade == c:
+        pass
+    elif biology_grade == d:
+        pass
+    elif biology_grade == f:
+        pass
+    else:
+        print "Your grade is invalid"
