a61d64e223ddb05f108c8df400eafaba8d4789cb
dellbby <dellbby4@yahoo.com>
2015-08-20 18:00:47 -0700

    6

--- reusable-library/main.py
+++ reusable-library/main.py
@@ -6,12 +6,28 @@ DPWP
 '''
 
 import webapp2
-from pages import Page
+from pages import Page #from package import class
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = Page() #instance of page
+        page_head = p.head
+        page_form = p.form
+        page_close = p.close
+        page_result = p.result
 
+        if self.request.GET:
+            #stores info we got from form/information input
+            fname = self.request.GET['fname'] #firstname input
+            lname = self.request.GET['lname'] #lastname input
+
+            #below code is what prints out
+            self.response.write(page_head + page_result + '<h2> Hi ' + fname + ' ' + lname + ",</h2> " + page_close)
+        else:
+            self.response.write(page_head + page_form + page_close)
+            #print our info on page
+
+#do not touch
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
--- reusable-library/pages.py
+++ reusable-library/pages.py
@@ -1,23 +1,41 @@
+'''
+Delia Akbari
+August 20, 2015
+Reusable Library
+DPWP
+'''
+
 class Page(object):
     def __init__(self):
-        self.__title = "Calculate your GPA"
-        self.css = "css/style.css"
-        self.__head = """
+        #start Doctype html code - header, form,result,etc.
+    	self.head = """
 <!DOCTYPE HTML>
-<HTML>
-        <HEAD>
-                <TITLE>What is your GPA?</Title>
-                <link href="css/style.css" rel="stylesheet" type="text/css" />
-        </head>
-        <body>
+<html>
+    <head>
+        <title>What is your GPA?</title>
+        <link href = "css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
         """
-        self.body = ""
-        self.__error = ''
-        self.__close = """
+
+        self.form = """
+        <header>
+            <h1>What is your Grade Point Average?</h1>
+        </header>
+        <div class="form"><form method="GET" action="">
+        <h2>Fill out the form below to find out</h2>
+        <label>First Name: </label><input type="text" name="fname" /><br>
+        <label>Last Name: </label><input type="text" name="lname" /><br>
+        <label></label><input type="submit" class="button" value="submit" /></div>
+        """
+
+        self.result = """
+        <link href = "css/styles.css" rel="stylesheet" type="text/css"/>
+        """
+
+        self.close = """
+        <HR>
+
         </body>
 </html>
         """
-
-    def print_out(self):
-        all = self.__head + self.body + self.__error + self.__close
-        return all
