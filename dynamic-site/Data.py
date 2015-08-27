0ad18bf408f08d99a7fc275992e43b2f4bf3e0da
dellbby <dellbby4@yahoo.com>
2015-08-27 00:14:27 -0700

    k

--- dynamic-site/Data.py
+++ dynamic-site/Data.py
@@ -4,6 +4,33 @@ Dynamic Site
 August 26, 2015
 '''
 
-class Data
+class Data(object):
     def __init__(self):
-        clothing = 
+        #data & attributes for each objects - clothing pieces with information
+
+        #AztecCropTop
+        aztec = Clothing()
+        aztec.price = '$28.00'
+        aztec.size = 'Small, Medium, Large'
+        aztec.make = '100% Rayon'
+        aztec.color = 'Mixed / Pattern'
+        aztec.style = 'Crop Top'
+        aztec.image = 'images/aztec.png'
+
+        #ChanelDripTop
+        chanel = Clothing()
+        chanel.price = '$40.00'
+        chanel.size = 'Small, Medium, Large'
+        chanel.make = '95% Rayon, 5% Spandex'
+        chanel.color = 'White & Black'
+        chanel.style = 'Crop Top'
+        chanel.image = 'images/chanel.png'
+
+        #ChanelDripTop
+        chanel = Clothing()
+        chanel.price = '$40.00'
+        chanel.size = 'Small, Medium, Large'
+        chanel.make = '95% Rayon, 5% Spandex'
+        chanel.color = 'White & Black'
+        chanel.style = 'Crop Top'
+        chanel.image = 'images/chanel.png'
